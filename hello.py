4ee23c9420ae9037dc6b2cae7534bc3c10eed5a6
Muciej <kejdzuuu@gmail.com>
2019-07-01 22:56:31 +0200

    Fixed typo, added new dynamic page

--- hello.py
+++ hello.py
@@ -1,9 +1,14 @@
 from flask import Flask
-app = flask(__name__)
+app = Flask(__name__)
 
 @app.route('/')
 def index():
     return '<h1>Hello world!</h1>'
+
+@app.route('/user/<name>')
+def user(name):
+    return '<h1>Hello, {}!</h1>'.format(name)
+
 
 if __name__ == '__main__':
     app.run(port=4000, debug=True)
